0f113aea7a305e3a1deafc23d518301d54dccdf9
Piotr Sikora <mlody92@users.noreply.github.com>
2019-07-07 20:39:35 +0200

    add client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,64 @@ import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
+import UserList from "./user/UserList";
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+interface User {
+    id: number;
+    email: string;
+}
+
+interface AppProps {
+}
+
+interface AppState {
+    users: Array<User>;
+    isLoading: boolean;
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+    constructor(props: AppProps) {
+        super(props);
+
+        this.state = {
+            users: [],
+            isLoading: false
+        };
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+
+        fetch('http://localhost:8080/users')
+            .then(response => response.json())
+            .then(data => this.setState({users: data, isLoading: false}));
+    }
+
+    render() {
+    // public render() {
+
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to React</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //   </div>
+    // );
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <UserList/>
+            </div>
+        );
   }
 }
 
--- server/src/main/java/com/arokis/share/user/UserRestController.java
+++ server/src/main/java/com/arokis/share/user/UserRestController.java
@@ -5,6 +5,7 @@ import com.arokis.share.user.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class UserRestController {
     private UserRepository userRepository;
 
     @GetMapping("/users")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<User> getAllUser(Pageable pageable) {
         Page<User> all = userRepository.findAll(pageable);
         return all.getContent();
